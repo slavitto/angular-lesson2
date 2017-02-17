1a3ed0c67ac574cdc9f6377c7efd33d4df09e578
slavitto <slavitto@gmail.com>
2017-02-17 10:07:27 +0300

    added separate menu and myAccount

--- app/app.js
+++ app/app.js
@@ -5,31 +5,21 @@ angular.module('myApp', [
     'ui.router',
     'ngMessages'
 ]).
+
 config(function($stateProvider) {
 
     $stateProvider
-        .state({
-            name: 'list',
-            url: '/list',
-            templateUrl: 'PokemonList/PokemonList.html',
-            controller: 'PokemonListCtrl as vm'
-        })
-        .state({
-            name: 'createNewPokemon',
-            url: '/new',
-            templateUrl: 'CreatePokemon/CreatePokemon.html',
-            controller: 'CreatePokemonCtrl as vm'
-        })
         .state({
             name: 'detail',
             url: '/pokemons/:pokemonId',
             templateUrl: 'PokemonDetail/PokemonDetail.html',
             controller: 'PokemonDetailCtrl as vm'
-        }).state({
+        })
+        .state({
             name: 'detail.edit',
             url: '/edit',
             parent: 'detail',
             templateUrl: 'PokemonDetail/PokemonDetailEdit.html',
             controller: function() {}
-        });
+        })
 });
